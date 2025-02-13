8aa9503d89f0251fc49610df3f49917fc8f1e0db
Bigtruck2 <bigburrito363@gmail.com>
2025-02-13 11:24:49 -0500

    initial

---
+++ src/Data.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class Data
+{
+    public static final int MAX = 1000 /* value not shown */;
+    private int[][] grid = {{4,3,4},{3,4,5},{4,5,6}};
+    /** Fills all elements of grid with randomly generated values, as described in part (a)
+     * Precondition: grid is not null.
+     * grid has at least one element.
+     */
+    public void repopulate()
+    {
+        for(int i = 0; i<grid.length;i++){
+            for(int j = 0; j<grid[0].length;j++) {
+                int rand = 100;
+                while (rand % 100 == 0) {
+                    rand = (int) (Math.random() * ((double) (MAX + 1) / 10)) * 10;
+                }
+                grid[i][j] = rand;
+            }
+        }
+        System.out.println(Arrays.deepToString(grid));
+    }
+    /** Returns the number of columns in grid that are in increasing order, as described
+     * in part (b)
+     * Precondition: grid is not null.
+     * grid has at least one element.
+     */
+    public int countIncreasingCols()
+    { /* to be implemented in part (b) */
+        //row col
+        int count = 0;
+        for(int j = 0; j<grid[0].length;j++) {
+            for(int i = 0; i<grid.length-1;i++){
+                if(grid[j][i]<=grid[j][i+1]) return count;
+            }
+            count++;
+        }
+        return 0;
+    }
+// There may be instance variables, constructors, and methods that are not shown.
+}
